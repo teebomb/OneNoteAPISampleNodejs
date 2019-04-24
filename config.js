453dbcf9030d3bde33e8c311cf75f69b929e6f63
teebomb <30992419+teebomb@users.noreply.github.com>
2019-04-24 11:03:58 -0500

    Update config.js
    Updated per Load Loader App

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     //Live Connect API information
-    clientId: '000000004011C945',
-    clientSecret: 'U5ofKaQiol75VbohV4hh-2x8XZpcJoPf ',
-    redirectUrl: 'http://onenoteapisamples.com:3000/callback'
+    clientId: '8959c78b-fafa-4d77-87ae-01013746b682',
+    clientSecret: 'maicaZEO540-kzHHGN65*?}',
+    redirectUrl: 'https://www.google.com/search?q=OAUTH+AJAX&oq=OAUTH+AJAX&aqs=chrome..69i57.15862j0j7&sourceid=chrome&ie=UTF-8'
 };
