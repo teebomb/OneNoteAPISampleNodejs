715592453eaeea7e9884c40fb201d05d82c37bc3
teebomb <30992419+teebomb@users.noreply.github.com>
2019-04-24 13:04:20 -0500

    BACK TO DEFAULT

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     //Live Connect API information
     clientId: '8959c78b-fafa-4d77-87ae-01013746b682',
     clientSecret: 'maicaZEO540-kzHHGN65*?}',
-    redirectUrl: 'https://www.google.com/search?q=OAUTH+AJAX&oq=OAUTH+AJAX&aqs=chrome..69i57.15862j0j7&sourceid=chrome&ie=UTF-8'
+    redirectUrl: 'http://onenoteapisamples.com:3000/callback'
 };
